497e71df57d9d76a3a4ad6709553a5ba96a42246
kimsoryeon <kimsr9210@naver.com>
2024-11-11 15:42:41 +0900

    커밋

---
+++ src/main/java/com/example/leavecalculationsystem/LeaveCalculationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.leavecalculationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveCalculationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveCalculationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leavecalculationsystem/LeaveCalculationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leavecalculationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveCalculationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
