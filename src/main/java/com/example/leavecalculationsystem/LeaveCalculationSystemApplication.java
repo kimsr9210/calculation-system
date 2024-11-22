a10bc00af1720965d59eac23d705474943a60091
kimsoryeon <kimsr9210@naver.com>
2024-11-22 17:50:23 +0900

    커밋

--- src/main/java/com/example/leavecalculationsystem/LeaveCalculationSystemApplication.java
+++ src/main/java/com/example/leavecalculationsystem/LeaveCalculationSystemApplication.java
@@ -8,6 +8,7 @@ public class LeaveCalculationSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LeaveCalculationSystemApplication.class, args);
+        //테스트
     }
 
 }
